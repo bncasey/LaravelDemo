25415fbdd9185a6c1867d9a2d050b4f523eb8612
bryce casey <brycecasey@bryces-iMac.attlocal.net>
2021-08-13 12:05:15 -0400

    Added tailwindcss to the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,90 +3,102 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css')}}" rel="stylesheet">
         <title>Bryce Casey</title>
 
 
         <!-- Styles -->
-<style>
 
-@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,100&display=swap');
+        <style>
 
-body {
-      font-family: sans-serif;
-      text-align: center;
-              }
+        @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,100&display=swap');
 
-h1 {
-   text-align: center;
-   border-style: solid;
-   font-family: cursive;
+        body {
+              font-family: sans-serif;
+              text-align: center;
+                      }
 
-}
-
-h2 {
-      text-align: left;
-      text-decoration: underline;
-      padding: 10px;
-        }
-
-p {
-      text-align: left;
-      padding: 10px;
+        h1 {
+           text-align: center;
+           border-style: solid;
+           font-family: cursive;
 
         }
 
-h3 {
-      text-align: left;
-      text-decoration: underline;
+        h2 {
+              text-align: left;
+              text-decoration: underline;
+              padding: 10px;
+                }
+
+        p {
+              text-align: left;
+              padding: 10px;
+
+                }
+
+        h3 {
+              text-align: left;
+              text-decoration: underline;
+                }
+
+        p2 {
+              text-align: left;
+                }
+
+        h4 {
+          text-align: left;
+          text-decoration: underline;
+
         }
 
-p2 {
-      text-align: left;
+        ul {
+          text-align: left;
         }
 
-h4 {
-  text-align: left;
-  text-decoration: underline;
-
-}
-
-ul {
-  text-align: left;
-}
-
-  </style>
-
+    </style>
 </head>
 
-<body style= "background-Color:#147CD8;">
+** Beginning**
 
-  <div class="me">
-
-<h1>Welcome to Bryce Casey's webpage</h1>
-
-<h2>About me</h2>
-    <p style="color:white;"> <b>My name is Bryce Casey and I am a Web Developer. I grew up in Long Island and recently just moved to South Carolina. In the year of 2015 I attended SUNY Cortland with a degree in Business Economics and recieved a minor in Computer Applications. During my time enrolled I was a front end intern for CladNetwork. I am result oriented and love to collaborate with developers to contribute to mass projects and client needs.</b></p>
-
-
- <h3>Skills</h3>
-    <p2 style="Color:white;"><b>
-      <ul>
-        <li> HTML & CSS
-          <li>Python
-            <li>Responsive
-              <li> SEO
-                <li> 3D printing
-            </ul></p2>
+<div>
+<body class="bg-indigo-200 font-sans-serif">
+    <h1 class=" font-cursive text-4xl text-blue-500 font-bold py-10 text-center underline" >
+    Welcome to Bryce Casey's webpage
+    </h1>
+</div>
 
 
-  <h4><i>Contact Information<i> </h4>
-    <ul style="color:white;"><B>
-      <li> bncasey20@gmail
-        <li> 631-905-1177
-          <li> <a href="www.linkedin.com/in/bryce-casey-031207174">Linked In</a>
-    </ul>
-          </div>
+**Section 1**
+<div class="text-left mt-20 ml-10">
+    <h3 class="text-2xl text-white underline">About me</h3>
+    <p class="text-white"> <b>My name is Bryce Casey and I am a Web Developer. I grew up in Long Island and recently just moved to South Carolina. In the year of 2015 I attended SUNY Cortland with a degree in Business Economics and recieved a minor in Computer Applications. During my time enrolled I was a front end intern for CladNetwork. I am result oriented and love to collaborate with developers to contribute to mass projects and client needs.</b></p>
+</div>
 
-        </body>
+**Section 2**
+<div class="text-left text-white ml-10 mt-20 ">
+  <h3 class="text-2xl">Skills</h3>
+  <p2><b>
+  <ul>
+      <li> HTML & CSS
+      <li>Python
+      <li>Responsive
+      <li> SEO
+      <li> 3D printing
+  </ul></p2>
+</div>
 
-    </html>
+**Section 3**
+<div class="text-left text-white ml-10 mt-20">
+  <h3 class="italic text-2xl">Contact Information </h3>
+  <ul>
+    <li> bncasey20@gmail
+    <li> 631-905-1177
+    <li> <a href="www.linkedin.com/in/bryce-casey-031207174">Linked In</a>
+  </ul>
+</div>
+
+
+  </body>
+
+</html>
